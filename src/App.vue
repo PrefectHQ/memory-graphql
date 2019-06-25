1a4d4450dcaa84538ecb055cb101219cfb530e97
Jenise Walters <jenisewalters@Jenises-MBP.fios-router.home>
2019-06-25 15:04:49 -0400

    get cards from graphql

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/MemoryGame.vue";
 
 export default {
   name: "app",
---
+++ src/components/MemoryGame.vue
@@ -0,0 +1,38 @@
+<template>
+   <p><pre>{{cards}}</pre></p>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    cards: gql`query {
+      cards {
+          id
+          value 
+          suit
+      }
+    }`,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
